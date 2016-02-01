7bb3044c1b3cf1f4c655c3b6395420ab6b4c8c21
bill <zhangjingle.love@163.com>
2016-02-02 01:54:01 +0800

    modify strlen.c

--- ptr_array/strlen.c
+++ ptr_array/strlen.c
@@ -4,11 +4,39 @@
 
 int strlen_( char s[])
 {
+	int n = 0;
+	int i = 0 ;
+	char *p = s;
+	//for( n = 0; *s != '\0'; s++,n++); 1
+	//for ( i = 0; s[i] != '\0'; i++);	2
+	
+	//for( ;*p != '\0'; p++); 3
+	//	return p - s;
+
+	//for ( i= 0 ; p[i] != '\0'; i++ ); 4
+	//	return i;
+	
+	
+	//while( *s++ && ++n ); 5
+	
+	//while( s[i++] ); 6
+	//	return i - 1; 
+	
+		
+	
+	//return n;
+	//return i;
+}
+
+int strlen_2( char s[])
+{
 	int n;
 	for( n = 0; *s != '\0'; s++,n++);
 
 	return n;
 }
+
+
 int main(void)
 {
 	int len;
