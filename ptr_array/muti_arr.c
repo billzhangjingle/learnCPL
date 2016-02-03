f77a18f3b78bde37d29f424adc36e19ce1b98ac1
bill <zhangjingle.love@163.com>
2016-02-03 17:38:09 +0800

    书中的写法，简单，不用重新定义变量

--- ptr_array/muti_arr.c
+++ ptr_array/muti_arr.c
@@ -13,14 +13,14 @@ int day_of_year( int year, int m, int d )
 	//判断失败条件
 	//mounth也要判断，是否有那么多天
 	
-	int day ;
+	//int day ;
 	int leaf;
 	int i;
-	
+
+#if 0	
 	//又是忘记初始化造成的错误，导致打印出来的数字很大
-	//day = 0;
+	day = 0;
 	leaf = ( year%4 == 0 && year %100 != 0) || ( year%400 == 0 );
-	
 	if ( d > m_day[leaf][m] ){
 		printf("err day\n");
 		return -1;
@@ -29,7 +29,20 @@ int day_of_year( int year, int m, int d )
 		day += m_day[leaf][i-1];
 	
 	day += d;
-	return day; 
+#endif
+
+	//这样就可以不用重新定义一个变量
+	leaf = ( year%4 == 0 && year %100 != 0) || ( year%400 == 0 );
+	
+	if ( d > m_day[leaf][m] ){
+		printf("err day\n");
+		return -1;
+	}
+
+	for ( i = 1; i < m; i++ );
+		d += m_day[leaf][i-1];
+	
+	return d; 
 }
 
 int mounth_day()
