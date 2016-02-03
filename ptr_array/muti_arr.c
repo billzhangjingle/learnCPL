03054f070f6640fee620ac6306a08d23356e5abe
bill <zhangjingle.love@163.com>
2016-02-03 18:23:02 +0800

    完成了 输入一年中的数字，返回出是某一个月的某一天

--- ptr_array/muti_arr.c
+++ ptr_array/muti_arr.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-static char m_day[2][12] = {
-{31,28,31,30,31,30,31,31,30,31,30,31},
-{31,29,31,30,31,30,31,31,30,31,30,31},
+static char m_day[2][13] = {
+{0,31,28,31,30,31,30,31,31,30,31,30,31},
+{0,31,29,31,30,31,30,31,31,30,31,30,31},
 };
 
 
@@ -40,20 +40,40 @@ int day_of_year( int year, int m, int d )
 	}
 
 	for ( i = 1; i < m; i++ );
-		d += m_day[leaf][i-1];
+		d += m_day[leaf][i];
 	
 	return d; 
 }
 
-int mounth_day()
+int mounth_day( int year, int day, int *m, int *d ) 
 {
+	int leaf;	
+	int i;
 	
+	leaf = ( year%4 == 0 && year %100 != 0) || ( year%400 == 0 );
+	
+	if ( (leaf && day > 366) ||( !leaf && day > 365) )		return -1;
+	
+	//只是实现功能， 这肯定是自己的思路，
+	//判断停止条件时，用day >0 来判断，
+	//太傻了，怪不得写不出代码来。 
+	for ( i = 1; day > 0 ; i++ )
+		day -= m_day[leaf][i];
+	*m = i - 1 ;
+	*d = day + m_day[leaf][i-1] ;
+
+	return 0;
 } 
 
 int main( void )
 {
 	int day;
+	int m, d;
 	day = day_of_year( 2000, 2, 23 );
 	printf("%d\n", day);	
+	//mounth_day( 1999, 60,&m, &d );
+	mounth_day( 2000, 60,&m, &d );
+	
+	printf("m:%d\tday:%d\n", m, d);
 	return 0;
 }
