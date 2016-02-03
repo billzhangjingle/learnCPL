9c2305bd25c94eb1649d7fbddf9595bd7c57bab2
bill <zhangjingle.love@163.com>
2016-02-03 17:12:36 +0800

    add muti_arr.c year of day,but has some mistack

---
+++ ptr_array/muti_arr.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char m_day[2][12] = {
+{31,28,31,30,31,30,31,31,30,31,30,31},
+{31,29,31,30,31,30,31,31,30,31,30,31},
+};
+
+
+int day_of_year( int year, int m, int d )
+{
+	//判断失败条件
+	//mounth也要判断，是否有那么多天
+	
+	int day ;
+	int leaf;
+	int i;
+	leaf = ( year%4 == 0 && year %100 != 0) || ( year%400 == 0 );
+	
+	if ( d > m_day[leaf][m] ){
+		printf("err day\n");
+		return -1;
+	}
+	for ( i = 1; i < m; i++ );
+		day += m_day[leaf][i-1];
+	
+	day += d;
+	return day; 
+}
+int main( void )
+{
+	int day;
+	day = day_of_year( 2000, 2, 23 );
+	printf("%d\n", day);	
+	return 0;
+}
